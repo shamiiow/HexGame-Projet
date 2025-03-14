d77a41991bef1f53249a9b86a1667f52d6a8a9eb
LGressier <102920045+LGressier@users.noreply.github.com>
2025-03-14 18:15:26 +0100

    jsp

--- test reel/network.py
+++ test reel/network.py
@@ -5,8 +5,8 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "127.0.0.1"
-        self.port = 5555
+        self.server = "176.136.110.6"
+        self.port = 65432
         self.addr = (self.server, self.port)
         self.pos = self.connect()
 
--- waiting_screen.py
+++ waiting_screen.py
@@ -36,59 +36,69 @@ class RoundedButton(Button):
         self.bg_rect.pos = [self.pos[0]-13, self.pos[1]-6]
         self.bg_rect.size = [self.size[0]+26, self.size[1]+12]
 
+class CloseButton(Button):
+    def __init__(self, **kwargs):
+        super(CloseButton, self).__init__(**kwargs)
+        self.background_normal = ''
+        self.background_down = ''
+        self.background_color = (100 / 255, 45 / 255, 90 / 255, 1)
+        with self.canvas.before:
+            Color(0 / 255, 45 / 255, 90 / 255, 1)
+            self.bg_rect = RoundedRectangle(size=self.size, pos=self.pos, radius=[30])
+        self.bind(pos=self.update_bg_rect, size=self.update_bg_rect)
+
+    def update_bg_rect(self, *args):
+        self.bg_rect.pos = [self.pos[0]-13, self.pos[1]-6]
+        self.bg_rect.size = [self.size[0]+26, self.size[1]+12]
+        print(self.bg_rect.pos, self.bg_rect.size)
+
+def close_app(instance):
+    App.get_running_app().stop()
+
 class WaitingScreen(Screen):
     def __init__(self, **kwargs):
         super(WaitingScreen, self).__init__(**kwargs)
-        Window.clearcolor = (240/255, 219/255, 175/255, 1)
+        Window.clearcolor = (240 / 255, 219 / 255, 175 / 255, 1)
 
         self.grille = 7
 
-        self.root = AnchorLayout()
+        self.root = AnchorLayout(anchor_x='left', anchor_y='top')
 
-        # Ajout de l'arrière-plan
         with self.root.canvas.before:
             self.rects = []
             self.update_gradient()
 
         self.root.bind(size=self.update_rect, pos=self.update_rect)
 
-        # Layout principal pour le contenu
         self.content_layout = BoxLayout(orientation='vertical', padding=[30, 70, 30, 30], spacing=10)
-
-        # Titre
-        self.title = Label(text='Waiting Screen ...', font_size=90, color=(0/255, 20/255, 40/255, 1))
+        self.title = Label(text='Waiting Screen ...', font_size=90, color=(0 / 255, 20 / 255, 40 / 255, 1))
         self.content_layout.add_widget(self.title)
 
-        # Espacement entre le titre et les boutons
         self.spacer = Widget(size_hint=(1, 0.5))
         self.content_layout.add_widget(self.spacer)
 
-        self.texte = Label(text="Pick the grid size :", font_size=40, color=(0/255, 20/255, 40/255, 1))
+        self.texte = Label(text="Pick the grid size :", font_size=40, color=(0 / 255, 20 / 255, 40 / 255, 1))
         self.content_layout.add_widget(self.texte)
 
-        # Boutons et compteur
         self.button_layout = BoxLayout(orientation='horizontal', size_hint=(1, 1), padding=[150, 0, 150, 20], spacing=20)
-
         self.decrement_button = RoundedButton(text='-', font_size=60, size_hint=(0.3, 1))
         self.decrement_button.bind(on_press=self.decrement_grille)
-
-        self.grille_label = ColoredLabel(text=str(self.grille), color=(0/255, 45/255, 90/255, 1), font_size=60, size_hint=(0.4, 1))
-
+        self.grille_label = ColoredLabel(text=str(self.grille), color=(0 / 255, 45 / 255, 90 / 255, 1), font_size=60, size_hint=(0.4, 1))
         self.increment_button = RoundedButton(text='+', font_size=60, size_hint=(0.3, 1))
         self.increment_button.bind(on_release=self.increment_grille)
 
         self.button_layout.add_widget(self.decrement_button)
         self.button_layout.add_widget(self.grille_label)
         self.button_layout.add_widget(self.increment_button)
-
-        # Ajouter le layout des boutons au layout principal
         self.content_layout.add_widget(self.button_layout)
 
-        # Ajouter le layout principal au root layout
         self.root.add_widget(self.content_layout)
-
-        # Ajouter le root layout à la fenêtre
         self.add_widget(self.root)
+
+        # Ajout du bouton "X" arrondi
+        self.close_button = CloseButton(text='Back', size_hint = (None, None), size = (70, 30), pos_hint = (None, None), pos=(10,60))
+        self.close_button.bind(on_press=close_app)
+        self.root.add_widget(self.close_button)
 
     def update_rect(self, *args):
         self.update_gradient()
@@ -96,11 +106,8 @@ class WaitingScreen(Screen):
     def update_gradient(self, *args):
         self.root.canvas.before.clear()
         with self.root.canvas.before:
-            # Définir les couleurs de départ et de fin du dégradé
-            color_start = Color(240/255, 219/255, 175/255, 1)  
-            color_end = Color(225/255, 156/255, 144/255,1)   
-
-            # Dessiner le dégradé
+            color_start = Color(240 / 255, 219 / 255, 175 / 255, 1)
+            color_end = Color(225 / 255, 156 / 255, 144 / 255, 1)
             self.rects = []
             for i in range(100):
                 color = Color(
@@ -113,11 +120,9 @@ class WaitingScreen(Screen):
                 self.rects.append(rect)
 
     def increment_grille(self, instance):
-        self.grille = min(self.grille+1, 17)
+        self.grille = min(self.grille + 1, 17)
         self.grille_label.text = str(self.grille)
 
     def decrement_grille(self, instance):
-        self.grille = max(self.grille-1, 2)
+        self.grille = max(self.grille - 1, 2)
         self.grille_label.text = str(self.grille)
-
-
